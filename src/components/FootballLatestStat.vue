420b3f7e3856eb16fe92163962b76bb1f21a6666
Stine Julius <stine.insecure@protonmail.com>
2025-04-28 13:57:11 +0200

    Created game stats section
    It works!

--- src/components/FootballLatestStat.vue
+++ src/components/FootballLatestStat.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { ref } from 'vue';
 
-const homeTeamName = ref(null);
-const homeTeamLogo = ref(null);
-const homeTeamScore = ref(null);
-const awayTeamName = ref(null);
-const awayTeamLogo = ref(null);
-const awayTeamScore = ref(null);
+// const homeTeamName = ref(null);
+// const homeTeamLogo = ref(null);
+// const homeTeamScore = ref(null);
+// const awayTeamName = ref(null);
+// const awayTeamLogo = ref(null);
+// const awayTeamScore = ref(null);
 
 // const homeTeamName2 = ref(null);
 // const homeTeamScore2 = ref(null);
@@ -37,7 +37,7 @@ const props = defineProps({
         required: true,
     },
     homeTeamLogo: {
-        default: null,
+        default: null, // Sættes da man ikke kan være sikker på at det findes i API
     },
     homeTeamScore: {
         type: Number,
@@ -48,7 +48,7 @@ const props = defineProps({
         required: true,
     },
     awayTeamLogo: {
-        default: null,
+        default: null, // Sættes da man ikke kan være sikker på at det findes i API
     },
     awayTeamScore: {
         type: Number,
@@ -61,36 +61,36 @@ const props = defineProps({
 
 
 
-async function GetData() {
-    let response = await fetch("https://v3.football.api-sports.io/fixtures?league=120&season=2024&team=405&last=5", {
-        method: "GET",
-        headers: {
-            "x-apisports-key": "10c1dce6b1c7f7e049a53bdbc7f8d164",
-        },
-    })
-    let result = await response.json();
+// async function GetData() {
+//     let response = await fetch("https://v3.football.api-sports.io/fixtures?league=120&season=2024&team=405&last=5", {
+//         method: "GET",
+//         headers: {
+//             "x-apisports-key": "10c1dce6b1c7f7e049a53bdbc7f8d164",
+//         },
+//     })
+//     let result = await response.json();
 
-    console.log(result);
+//     console.log(result);
 
-    const gameObject1 = result.response[0];
-    const gameObject2 = result.response[1];
-    const gameObject3 = result.response[2];
-    const gameObject4 = result.response[3];
-    const gameObject5 = result.response[4];
+//     const gameObject1 = result.response[0];
+//     const gameObject2 = result.response[1];
+//     const gameObject3 = result.response[2];
+//     const gameObject4 = result.response[3];
+//     const gameObject5 = result.response[4];
 
-    // 1. Game info
-    homeTeamName1.value = gameObject1.teams.home.name;
-    homeTeamScore1.value = gameObject1.goals.home;
-    awayTeamName1.value = gameObject1.teams.away.name;
-    awayTeamScore1.value = gameObject1.goals.away;
+//     // 1. Game info
+//     homeTeamName1.value = gameObject1.teams.home.name;
+//     homeTeamScore1.value = gameObject1.goals.home;
+//     awayTeamName1.value = gameObject1.teams.away.name;
+//     awayTeamScore1.value = gameObject1.goals.away;
 
-    homeTeamName.value = [
-        gameObject1.teams.home.name, 
-        gameObject2.teams.home.name, 
-        gameObject3.teams.home.name, 
-        gameObject4.teams.home.name, 
-        gameObject5.teams.home.name, 
-    ]
+//     homeTeamName.value = [
+//         gameObject1.teams.home.name, 
+//         gameObject2.teams.home.name, 
+//         gameObject3.teams.home.name, 
+//         gameObject4.teams.home.name, 
+//         gameObject5.teams.home.name, 
+//     ]
 
 
     // En for loop, looper igennem et array med alle kamp informationerne (for de 5 sidste kampe), 
@@ -102,9 +102,9 @@ async function GetData() {
 
 
 
-}
+// }
 
-GetData();
+// GetData();
 
 
 
@@ -116,20 +116,20 @@ GetData();
 <template>
     <div id="game-stat">
         <div class="team-info">
-            <img :src="homeTeamLogo" alt="Team logo">
+            <img v-if="props.homeTeamLogo" :src="homeTeamLogo" alt="Hjemmehold logo">
             <p class="bold"> {{ homeTeamName }} </p>
         </div>
         <div>
-            <h2> {{ homeTeamScore }} </h2>
+            <h2> {{ props.homeTeamScore !== null ? props.homeTeamScore : '-' }} </h2>
         </div>
         <div>
             <h2 class="bold"> - </h2>
         </div>
         <div>
-            <h2> {{ awayTeamScore }} </h2>
+            <h2> {{ props.awayTeamScore !== null ? props.awayTeamScore : '-' }} </h2>
         </div>
         <div class="team-info">
-            <img :src="awayTeamLogo" alt="Team logo">
+            <img v-if="props.awayTeamLogo" :src="awayTeamLogo" alt="Udehold logo">
             <p class="bold"> {{ awayTeamName }} </p>
         </div>
     </div>
@@ -157,7 +157,7 @@ GetData();
 }
 
 .team-info>img {
-    background-color: aquamarine;
+    /* background-color: aquamarine; */
     max-width: 50px;
 }
 </style>
--- src/components/FootballLatestStats.vue
+++ src/components/FootballLatestStats.vue
@@ -1,15 +1,34 @@
 <script setup>
 import FootballLatestStat from './FootballLatestStat.vue'
+import { ref, onMounted } from 'vue';
+
+const footballGames = ref([]);
+
+async function GetGameData() {
+    let response = await fetch("https://v3.football.api-sports.io/fixtures?league=120&season=2024&team=405&last=5", {
+        method: "GET",
+        headers: {
+            "x-apisports-key": "10c1dce6b1c7f7e049a53bdbc7f8d164",
+        },
+    })
+    let result = await response.json();
+
+    console.log(result);
+
+    footballGames.value = result.response;
+}
+
+onMounted(() => {
+    GetGameData();
+});
 </script>
 
 <template>
     <div id="stat-list">
         <h2> De 5 sidste kampe </h2>
-        <FootballLatestStat />
-        <FootballLatestStat />
-        <FootballLatestStat />
-        <FootballLatestStat />
-        <FootballLatestStat />
+        <FootballLatestStat v-for="game in footballGames" :key="game.fixture.id" :homeTeamName="game.teams.home.name"
+            :homeTeamLogo="game.teams.home.logo" :homeTeamScore="game.goals.home" :awayTeamName="game.teams.away.name"
+            :awayTeamLogo="game.teams.away.logo" :awayTeamScore="game.goals.away"/>
     </div>
 </template>
 
